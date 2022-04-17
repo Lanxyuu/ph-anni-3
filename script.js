5d6c2b72a3a90830c5dea1501c7abb301dd9cd36
Lanxyuu <69546927+Lanxyuu@users.noreply.github.com>
2022-04-17 17:46:49 -0400

    added card generator text, images, descriptions

--- script.js
+++ script.js
@@ -6,20 +6,16 @@ const meaning2 = document.getElementById("meaning2");
 const meaning3 = document.getElementById("meaning3");
 const btn = document.getElementById("btn");
 
-const cardImg = ["./cards/arcana/death.png", "./cards/arcana/judgement.png", "./cards/arcana/justice.png"];
-const upright = ["<b>upright death</b>: blah", "<b>upright judgement</b>: blah", "<b>upright justice</b>: blah"];
-const reversed = ["<b>reversed death</b>: blah", "<b>reversed judgement</b>: blah", "<b>reversed justice</b>: blah"];
-
 function chooseCard(cardId, meaningId) {
   const random = Math.floor(Math.random() * cardImg.length);
-  const isUpright = (0 == Math.round(Math.random())) ? true : false ;
-  cardId.src=cardImg[random];
+  const isUpright = (0 == Math.round(Math.random())) ? true : false;
+  cardId.src = cardImg[random];
   if (isUpright) {
     cardId.classList.remove("reversed");
-    meaningId.innerHTML=upright[random];
+    meaningId.innerHTML = upright[random];
   } else {
     cardId.classList.add("reversed");
-    meaningId.innerHTML=reversed[random];
+    meaningId.innerHTML = reversed[random];
   }
 }
 
@@ -28,3 +24,15 @@ let generateCards = () => {
   chooseCard(card2, meaning2);
   chooseCard(card3, meaning3);
 }
+
+
+
+const cardImg = ["./cards/arcana/death.png", "./cards/arcana/judgement.png", "./cards/arcana/justice.png"];
+
+const upright = ["<b>Death (Upright)</b>: transformation, endings, change, transition, letting go, release",
+  "<b>Judgement (Upright)</b>: self-evaluation, awakening, renewal, purpose, reflection, reckoning", 
+  "<b>Justice (Upright)</b>: justice, karma, consequence, accountability, law, truth, honesty, integrity, cause and effect"];
+
+const reversed = ["<b>Death (Reversed)</b>: fear of change, repeating negative patterns, resisting change, stagnancy, decay", 
+"<b>Judgement (Reversed)</b>: self-doubt, lack of self-awareness, failure to learn lessons, self-loathing", 
+"<b>Justice (Reversed)</b>: injustice, retribution, dishonesty, corruption, dishonesty, unfairness, avoiding accountability"];
