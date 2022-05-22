af0164607c8ae21bd907b609dc834d1ce9582370
Lanxyuu <69546927+Lanxyuu@users.noreply.github.com>
2022-05-21 23:49:29 -0400

    added modals

---
+++ modals.js
@@ -0,0 +1,253 @@
+document.getElementById("thefool").onclick = function(){
+    document.getElementById("thefool-modal").style.display = "block";
+}
+document.getElementById("themagician").onclick = function(){
+    document.getElementById("themagician-modal").style.display = "block";
+}
+document.getElementById("thehighpriestess").onclick = function(){
+    document.getElementById("thehighpriestess-modal").style.display = "block";
+}
+document.getElementById("theempress").onclick = function(){
+    document.getElementById("theempress-modal").style.display = "block";
+}
+document.getElementById("theemperor").onclick = function(){
+    document.getElementById("theemperor-modal").style.display = "block";
+}
+document.getElementById("theheirophant").onclick = function(){
+    document.getElementById("theheirophant-modal").style.display = "block";
+}
+document.getElementById("thelovers").onclick = function(){
+    document.getElementById("thelovers-modal").style.display = "block";
+}
+document.getElementById("thechariot").onclick = function(){
+    document.getElementById("thechariot-modal").style.display = "block";
+}
+document.getElementById("justice").onclick = function(){
+    document.getElementById("justice-modal").style.display = "block";
+}
+document.getElementById("thehermit").onclick = function(){
+    document.getElementById("thehermit-modal").style.display = "block";
+}
+document.getElementById("thewheeloffortune").onclick = function(){
+    document.getElementById("thewheeloffortune-modal").style.display = "block";
+}
+document.getElementById("strength").onclick = function(){
+    document.getElementById("strength-modal").style.display = "block";
+}
+document.getElementById("thehangedman").onclick = function(){
+    document.getElementById("thehangedman-modal").style.display = "block";
+}
+document.getElementById("death").onclick = function(){
+    document.getElementById("death-modal").style.display = "block";
+}
+document.getElementById("temperance").onclick = function(){
+    document.getElementById("temperance-modal").style.display = "block";
+}
+document.getElementById("thedevil").onclick = function(){
+    document.getElementById("thedevil-modal").style.display = "block";
+}
+document.getElementById("thetower").onclick = function(){
+    document.getElementById("thetower-modal").style.display = "block";
+}
+document.getElementById("thestar").onclick = function(){
+    document.getElementById("thestar-modal").style.display = "block";
+}
+document.getElementById("themoon").onclick = function(){
+    document.getElementById("themoon-modal").style.display = "block";
+}
+document.getElementById("thesun").onclick = function(){
+    document.getElementById("thesun-modal").style.display = "block";
+}
+document.getElementById("judgement").onclick = function(){
+    document.getElementById("judgement-modal").style.display = "block";
+}
+document.getElementById("theworld").onclick = function(){
+    document.getElementById("theworld-modal").style.display = "block";
+}
+
+document.getElementById("1ofcups").onclick = function(){
+    document.getElementById("1ofcups-modal").style.display = "block";
+}
+document.getElementById("2ofcups").onclick = function(){
+    document.getElementById("2ofcups-modal").style.display = "block";
+}
+document.getElementById("3ofcups1").onclick = function(){
+    document.getElementById("3ofcups1-modal").style.display = "block";
+}
+document.getElementById("3ofcups2").onclick = function(){
+    document.getElementById("3ofcups2-modal").style.display = "block";
+}
+document.getElementById("4ofcups").onclick = function(){
+    document.getElementById("4ofcups-modal").style.display = "block";
+}
+document.getElementById("5ofcups").onclick = function(){
+    document.getElementById("5ofcups-modal").style.display = "block";
+}
+document.getElementById("6ofcups").onclick = function(){
+    document.getElementById("6ofcups-modal").style.display = "block";
+}
+document.getElementById("7ofcups").onclick = function(){
+    document.getElementById("7ofcups-modal").style.display = "block";
+}
+document.getElementById("8ofcups").onclick = function(){
+    document.getElementById("8ofcups-modal").style.display = "block";
+}
+document.getElementById("9ofcups").onclick = function(){
+    document.getElementById("9ofcups-modal").style.display = "block";
+}
+document.getElementById("10ofcups").onclick = function(){
+    document.getElementById("10ofcups-modal").style.display = "block";
+}
+document.getElementById("pageofcups1").onclick = function(){
+    document.getElementById("pageofcups1-modal").style.display = "block";
+}
+document.getElementById("pageofcups2").onclick = function(){
+    document.getElementById("pageofcups2-modal").style.display = "block";
+}
+document.getElementById("knightofcups").onclick = function(){
+    document.getElementById("knightofcups-modal").style.display = "block";
+}
+document.getElementById("queenofcups").onclick = function(){
+    document.getElementById("queenofcups-modal").style.display = "block";
+}
+document.getElementById("kingofcups").onclick = function(){
+    document.getElementById("kingofcups-modal").style.display = "block";
+}
+
+document.getElementById("1ofpentacles").onclick = function(){
+    document.getElementById("aceofpentacles-modal").style.display = "block";
+}
+document.getElementById("2ofpentacles").onclick = function(){
+    document.getElementById("2ofpentacles-modal").style.display = "block";
+}
+document.getElementById("3ofpentacles").onclick = function(){
+    document.getElementById("3ofpentacles-modal").style.display = "block";
+}
+document.getElementById("4ofpentacles").onclick = function(){
+    document.getElementById("4ofpentacles-modal").style.display = "block";
+}
+document.getElementById("5ofpentacles").onclick = function(){
+    document.getElementById("5ofpentacles-modal").style.display = "block";
+}
+document.getElementById("6ofpentacles").onclick = function(){
+    document.getElementById("6ofpentacles-modal").style.display = "block";
+}
+document.getElementById("7ofpentacles").onclick = function(){
+    document.getElementById("7ofpentacles-modal").style.display = "block";
+}
+document.getElementById("8ofpentacles").onclick = function(){
+    document.getElementById("8ofpentacles-modal").style.display = "block";
+}
+document.getElementById("9ofpentacles").onclick = function(){
+    document.getElementById("9ofpentacles-modal").style.display = "block";
+}
+document.getElementById("10ofpentacles").onclick = function(){
+    document.getElementById("10ofpentacles-modal").style.display = "block";
+}
+document.getElementById("pageofpentacles").onclick = function(){
+    document.getElementById("pageofpentacles-modal").style.display = "block";
+}
+document.getElementById("knightofpentacles").onclick = function(){
+    document.getElementById("knightofpentacles-modal").style.display = "block";
+}
+document.getElementById("queenofpentacles").onclick = function(){
+    document.getElementById("queenofpentacles-modal").style.display = "block";
+}
+document.getElementById("kingofpentacles").onclick = function(){
+    document.getElementById("kingofpentacles-modal").style.display = "block";
+}
+
+document.getElementById("1ofswords").onclick = function(){
+    document.getElementById("1ofswords-modal").style.display = "block";
+}
+document.getElementById("2ofswords").onclick = function(){
+    document.getElementById("2ofswords-modal").style.display = "block";
+}
+document.getElementById("3ofswords").onclick = function(){
+    document.getElementById("3ofswords-modal").style.display = "block";
+}
+document.getElementById("4ofswords").onclick = function(){
+    document.getElementById("4ofswords-modal").style.display = "block";
+}
+document.getElementById("5ofswords").onclick = function(){
+    document.getElementById("5ofswords-modal").style.display = "block";
+}
+document.getElementById("6ofswords").onclick = function(){
+    document.getElementById("6ofswords-modal").style.display = "block";
+}
+document.getElementById("7ofswords").onclick = function(){
+    document.getElementById("7ofswords-modal").style.display = "block";
+}
+document.getElementById("8ofswords").onclick = function(){
+    document.getElementById("8ofswords-modal").style.display = "block";
+}
+document.getElementById("9ofswords").onclick = function(){
+    document.getElementById("9ofswords-modal").style.display = "block";
+}
+document.getElementById("10ofswords").onclick = function(){
+    document.getElementById("10ofswords-modal").style.display = "block";
+}
+document.getElementById("pageofswords").onclick = function(){
+    document.getElementById("pageofswords-modal").style.display = "block";
+}
+document.getElementById("knightofswords").onclick = function(){
+    document.getElementById("knightofswords-modal").style.display = "block";
+}
+document.getElementById("queenofswords").onclick = function(){
+    document.getElementById("queenofswords-modal").style.display = "block";
+}
+document.getElementById("kingofswords").onclick = function(){
+    document.getElementById("kingofswords-modal").style.display = "block";
+}
+
+document.getElementById("1ofwands").onclick = function(){
+    document.getElementById("1ofwands-modal").style.display = "block";
+}
+document.getElementById("2ofwands").onclick = function(){
+    document.getElementById("2ofwands-modal").style.display = "block";
+}
+document.getElementById("3ofwands").onclick = function(){
+    document.getElementById("3ofwands-modal").style.display = "block";
+}
+document.getElementById("4ofwands").onclick = function(){
+    document.getElementById("4ofwands-modal").style.display = "block";
+}
+document.getElementById("5ofwands").onclick = function(){
+    document.getElementById("5ofwands-modal").style.display = "block";
+}
+document.getElementById("6ofwands").onclick = function(){
+    document.getElementById("6ofwands-modal").style.display = "block";
+}
+document.getElementById("7ofwands").onclick = function(){
+    document.getElementById("7ofwands-modal").style.display = "block";
+}
+document.getElementById("8ofwands").onclick = function(){
+    document.getElementById("8ofwands-modal").style.display = "block";
+}
+document.getElementById("9ofwands").onclick = function(){
+    document.getElementById("9ofwands-modal").style.display = "block";
+}
+document.getElementById("10ofwands").onclick = function(){
+    document.getElementById("10ofwands-modal").style.display = "block";
+}
+document.getElementById("pageofwands").onclick = function(){
+    document.getElementById("pageofwands-modal").style.display = "block";
+}
+document.getElementById("knightofwands").onclick = function(){
+    document.getElementById("knightofwands-modal").style.display = "block";
+}
+document.getElementById("queenofwands").onclick = function(){
+    document.getElementById("queenofwands-modal").style.display = "block";
+}
+document.getElementById("kingofwands").onclick = function(){
+    document.getElementById("kingofwands-modal").style.display = "block";
+}
+
+window.onclick = function(event) {
+    if (event.target.className === "modal") {
+        var openModules = document.getElementsByClassName("modal");
+        for(var i = 0; i < openModules.length; i++){
+            openModules[i].style.display = "none";
+        }
+    }
+}
